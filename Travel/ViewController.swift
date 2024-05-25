43ae1968dc5337c816acc79fd3fa15578444e851
seongmin kim <k2417000@naver.com>
2024-05-25 21:20:25 +0900

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by 김성민 on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
