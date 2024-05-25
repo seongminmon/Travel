8c99c3bb62d9721e839615ad9c29da0f06daf085
seongmin kim <k2417000@naver.com>
2024-05-25 23:39:45 +0900

    여행 매거진 화면 구성하기

---
+++ Travel/TravelTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTableViewCell.swift
+//  Travel
+//
+//  Created by 김성민 on 5/25/24.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+    
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
