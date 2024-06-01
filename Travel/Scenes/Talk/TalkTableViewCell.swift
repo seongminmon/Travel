9f1e219e66673adeabb11ce32f1a1ce5a299535a
seongmin kim <k2417000@naver.com>
2024-06-01 16:44:29 +0900

    Talk 화면 구현

---
+++ Travel/Scenes/Talk/TalkTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  TalkTableViewCell.swift
+//  Travel
+//
+//  Created by 김성민 on 6/1/24.
+//
+
+import UIKit
+
+class TalkTableViewCell: UITableViewCell {
+
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var contentsLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureUI()
+    }
+    
+    func configureUI() {
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+//        profileImageView.backgroundColor = .gray
+        nameLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        contentsLabel.font = .systemFont(ofSize: 14)
+        contentsLabel.textColor = .lightGray
+        dateLabel.font = .systemFont(ofSize: 13)
+        dateLabel.textColor = .lightGray
+        dateLabel.textAlignment = .right
+    }
+    
+    func configureCell(_ data: ChatRoom) {
+        let imageName = data.chatroomImage.first ?? ""
+        profileImageView.image = UIImage(named: imageName)
+        nameLabel.text = data.chatroomName
+        contentsLabel.text = data.chatList.last?.message
+        dateLabel.text = data.chatList.last?.date
+    }
+}
---
+++ Travel/Scenes/Talk/TalkViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TalkViewController.swift
+//  Travel
+//
+//  Created by 김성민 on 6/1/24.
+//
+
+import UIKit
+
+class TalkViewController: UIViewController {
+
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var tableView: UITableView!
+    
+    var list = ChatRoom.mockChatList
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "TRAVEL TALK"
+        searchBar.placeholder = "친구 이름을 검색해보세요"
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = 80
+        
+        let xib = UINib(nibName: TalkTableViewCell.identifier, bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: TalkTableViewCell.identifier)
+    }
+}
+
+extension TalkViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TalkTableViewCell.identifier, for: indexPath) as! TalkTableViewCell
+        
+        let data = list[indexPath.row]
+        cell.configureCell(data)
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+}
